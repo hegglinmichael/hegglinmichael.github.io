804bb4b588f372dc9c1a4cd64d38ab681b8d265b
hegglinmichael <mhegglinmichael@gmail.com>
2025-06-03 14:19:59 -0700

    Update data.js

--- travel/data.js
+++ travel/data.js
@@ -1,8 +1,3 @@
-// List of ISO 3166-1 alpha-3 codes (e.g., "USA", "FRA")
-const visitedCountries = {
-  USA: "Trip to NYC and California in 2022.",
-  FRA: "Visited Paris and Nice in summer 2023.",
-  JPN: "Cherry blossom tour in 2024."
-};
-
-const wishlistCountries = ["NZL", "NOR", "ARG"];
+// Use ISO 3166-1 alpha-3 country codes
+const visited = ["USA", "FRA", "JPN"];
+const wishlist = ["NZL", "NOR", "ARG"];

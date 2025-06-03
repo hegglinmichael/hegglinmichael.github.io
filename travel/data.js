9c86d8bedbff90696273ea270f5212eee0539236
hegglinmichael <mhegglinmichael@gmail.com>
2025-06-03 14:14:57 -0700

    Create data.js
    data js file upload

---
+++ travel/data.js
@@ -0,0 +1,8 @@
+// List of ISO 3166-1 alpha-3 codes (e.g., "USA", "FRA")
+const visitedCountries = {
+  USA: "Trip to NYC and California in 2022.",
+  FRA: "Visited Paris and Nice in summer 2023.",
+  JPN: "Cherry blossom tour in 2024."
+};
+
+const wishlistCountries = ["NZL", "NOR", "ARG"];
